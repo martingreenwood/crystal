8c9edc70578a4327d3019bd272721bbf0e94c854
Martin Greenwood <martin@pixelpudu.com>
2017-03-14 14:08:16 +0000

    fix

--- functions.php
+++ functions.php
@@ -102,14 +102,11 @@ function crystal_scripts() {
 	wp_enqueue_style( 'crystal-style', get_stylesheet_uri() );
 	wp_enqueue_style( 'crystal-font', '//fonts.googleapis.com/css?family=Montserrat' );
 
+	wp_enqueue_script( 'jquery');
 	wp_enqueue_script( 'crystal-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '', true );
 	wp_enqueue_script( 'crystal-modernizr', get_template_directory_uri() . '/js/modernizr.js', array(), '', true );
 	wp_enqueue_script( 'crystal-parallax', get_template_directory_uri() . '/js/parallax.min.js', array(), '', true );
 	wp_enqueue_script( 'crystal-custom', get_template_directory_uri() . '/js/custom.js', array(), '', true );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
 }
 add_action( 'wp_enqueue_scripts', 'crystal_scripts' );
 
